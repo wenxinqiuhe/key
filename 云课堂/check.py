fda0811aa9238c99c2e1cc8846e6ab022d86aba2
wenxinqiuhe <71088709+wenxinqiuhe@users.noreply.github.com>
2021-06-04 22:22:15 +0800

    Add files via upload

--- 云课堂/check.py
+++ 云课堂/check.py
@@ -1,59 +1,65 @@
 from collections import namedtuple
 import requests
 import json
-#import os
-# os.chdir(os.path.dirname(__file__))
-from account import seq_se
+from account import *
 
-with open('cookie.json', 'r')as f:
-    cookie = json.load(f)
-header = seq_se().header
-url = 'http://gxic.itolearn.com/Checkin/CheckIn'
-data = {
-    'CheckInType': '1',
-    'Code': '158907'
-}
+
 s = requests.session()
-s.headers = header
-requests.utils.add_dict_to_cookiejar(s.cookies, cookie)
-r = s.post(url=url, data=data)
-addcookie = json.loads(r.text)
+s.headers = seq_se().header
+while True:
+    try:
+        with open('cookie.json', 'r')as f:
+            cookie = json.load(f)
+    except:
+        wr()
+        continue
+    requests.utils.add_dict_to_cookiejar(s.cookies, cookie)
+    r = s.post(url='http://gxic.itolearn.com/Checkin/CheckIn', data={'CheckInType': '1','Code': '741558'})
+    if r.status_code==200:
+        addcookie = json.loads(r.text)
+        break
+    else:
+        print('cookie失效')
+        wr()
 # 添加笔记
-addnote={"Content":".","IC_ID":addcookie['data']['IC_ID'],"ID":0,"userId":"52272721","fileID":[]}
-for x in range(5):
-	rr=s.post(url='http://gxic.itolearn.com/HomeNote/AddAnsterNote',data=addnote)
-	print(rr.text)
+icid = addcookie['data']['IC_ID']
+schduleid = addcookie['data']['Schedule_Id'][0]
+lessionid = addcookie['data']['Lessons_ID']
+addnote = {"Content": ".", "IC_ID": icid,"ID": 0, "userId": "52272721", "fileID": []}
+# for x in range(5):
+#     rr = s.post(url='http://gxic.itolearn.com/HomeNote/AddAnsterNote', data=addnote)
+#     print(rr.text)
 # 遍历课前课中课后
 for a in range(1, 4):
     postdata = {
         "applayType": a,
-        "ic_id": addcookie['data']['IC_ID'],
+        "ic_id": icid,
         "batchid": None,
-        "schduleid": addcookie['data']['Schedule_Id'][0],
-        "lessionid": addcookie['data']['Lessons_ID']
+        "schduleid": schduleid,
+        "lessionid": lessionid
     }
     headere = {
-        "icid": str(addcookie['data']['IC_ID']),
-        "scheduleid": str(addcookie['data']['Schedule_Id'][0]),
-        "lessionid": str(addcookie['data']['Lessons_ID'])
+        "icid": str(icid),
+        "scheduleid": str(schduleid),
+        "lessionid": str(lessionid)
     }
     requests.utils.add_dict_to_cookiejar(s.cookies, headere)
     cr = s.post('http://gxic.itolearn.com/Weixin/partialItemClassJson?v=60', data=postdata)
     list = json.loads(cr.text)
-    if len(list['items']) != 0:
+    if len(list['items']) != 0:#判断是否为空
+        name = {'覃福展': True, '黄善聪': True, '陆用炯': True, '邓深华': True,'何迎年': True, '杨帆': True, '莫国珩': True, '蒙炳松': True}
         for b in range(len(list['items'])):
+            dcidlist = []
             if list['items'][b]['typeId'] == '3':
-                quesion_url = 'http://gxic.itolearn.com/api/iCActivities'
+                print('3')
                 da = {
-                    'icid': str(addcookie['data']['IC_ID']),
+                    'icid': str(icid),
                     'taskIcaID': str(list['items'][b]['ID'])  # 题目id
                 }
                 co = {'token': requests.utils.dict_from_cookiejar(s.cookies)['token']}
                 s.headers.update(co)
-                studen_list = s.get(url=quesion_url, params=da)
+                studen_list = s.get(url='http://gxic.itolearn.com/api/iCActivities', params=da)
                 yun = json.loads(studen_list.text)
-                name = {'覃福展': True,'黄善聪': True,'陆用炯': True,'邓深华': True,'何迎年': True,'杨帆': True,'莫国珩': True,'蒙炳松': True}
-                dcidlist = []
                 for z in range(len(yun['data']['list'])):
                     result = name.get(yun['data']['list'][z]['OperName'])
                     if result == True:
@@ -64,20 +70,20 @@ for a in range(1, 4):
                     xb = s.post(url='http://gxic.itolearn.com/HomeNote/InsertNoteHome', data={'AP_ID': x, type: 3})
                     print(xb.status_code)
             if list['items'][b]['typeId'] == '5':
-                name = {'覃福展': True,'黄善聪': True,'陆用炯': True,'邓深华': True,'何迎年': True,'杨帆': True,'莫国珩': True,'蒙炳松': True}
-                dcidlist = []
+                 
                 if list['items'][b]['StuSubmitStatus'] == 0:
-                    # o=s.post('http://gxic.itolearn.com/Weixin/getmaterial',data=postjk)
-                    postjk={"courseElementId":str(list['items'][0]['courseElmentId'])}
+                    # postjkw={"courseElementId":str(list['items'][0]['courseElmentId'])}
+                    # postjkw.update(headere)
+                    # o3=s.post('http://gxic.itolearn.com/Weixin/getmaterial',data=postjkw)
+                    # print(o3.status_code,o3.text)学习资料相关的文档地址，id等数据
+                    postjk = {"courseElementId": str(list['items'][0]['courseElmentId'])}
                     o = s.get('http://gxic.itolearn.com/Weixin/material', params=postjk)
-                    print(o.text)
                     postdata2 = {
                         "id": list['items'][0]['courseElmentId'],
                         "content": "已阅读",
                         "Type": 2
                     }
-                    jr = s.post(
-                        'http://gxic.itolearn.com/webios/addComment', data=postdata2)
+                    jr = s.post('http://gxic.itolearn.com/webios/addComment', data=postdata2)
                     print(jr.status_code, jr.text)
                 for c in range(1, 6):
                     # postjk={"courseElementId":str(json.loads(cr.text)['items'][0]['courseElmentId'])}
@@ -88,18 +94,14 @@ for a in range(1, 4):
                         'page': c,
                         'pageSize': 10
                     }
-                    i = s.get(
-                        'http://gxic.itolearn.com/Weixin/getDiscussionContent', params=par)
+                    i = s.get(url='http://gxic.itolearn.com/Weixin/getDiscussionContent', params=par)
+                    getDis=json.loads(i.text)
                     try:
                         for d in range(10):
-                            result = name.get(json.loads(i.text)[
-                                              'list'][d]['OperName'])
+                            result = name.get(getDis['list'][d]['OperName'])
                             if result == True:
-                                dcidlist.append(json.loads(i.text)[
-                                                'list'][d]['DiscussionContent']['DC_ID'])
+                                dcidlist.append(getDis['list'][d]['DiscussionContent']['DC_ID'])
                     except:
                         pass
                 for e in dcidlist:
-                    ll = {'dcid': e, 'ica_type': 5, 'type': 1}
-                    des = s.post(
-                        'http://gxic.itolearn.com/webios/addAppreciation', data=ll)
+                    des = s.post(url='http://gxic.itolearn.com/webios/addAppreciation', data={'dcid': e, 'ica_type': 5, 'type': 1})
